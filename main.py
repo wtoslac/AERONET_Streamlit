19cf14c91b62089d793f912fea2c9dd824ac0494
Wing To <wto@csustan.edu>
2024-10-30 18:02:44 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,16 @@
-importing required libraries
-
 import streamlit as st
-
 from io import StringIO 
 
-
-
 #adding a file uploader
-
 file = st.file_uploader("Please choose a file")
-
 if file is not None:
-
     #To read file as bytes:
-
     bytes_data = file.getvalue()
-
     st.write(bytes_data)
-
-
-
     #To convert to a string based IO:
-
     stringio = StringIO(file.getvalue().decode("utf-8"))
-
     st.write(stringio)
-
-
-
     #To read file as string:
-
     string_data = stringio.read()
 
     st.write(string_data)
