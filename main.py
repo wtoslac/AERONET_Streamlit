69675743a8de5c7799ed2bd8406defecf7ecddc3
Wing To <wto@csustan.edu>
2024-10-30 18:03:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -23,16 +23,3 @@ if file is not None:
 
     st.write(df)
 
-
-
-#adding a file uploader to accept multiple CSV files
-
-uploaded_files = st.file_uploader("Please choose a CSV file", accept_multiple_files=True)
-
-for file in uploaded_files:
-
-    bytes_data = file.read()
-
-    st.write("File uploaded:", file.name)
-
-    st.write(bytes_data)
