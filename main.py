502a2271886360f2c82f960b8c3f479208c0cccf
Wing To <wto@csustan.edu>
2024-10-30 18:16:19 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,8 @@ import numpy as np
 import plotly.express as px
 import pandas as pd
 from io import StringIO 
-import numpy as np
-import plotly.express as px
 
 file = st.file_uploader("Please choose a file")
 df= pd.read_csv(file)
 plot = px.line(df, x = 'datetime', y = ['AOD_500nm'])
-st.plotly_chart(plot)
- 
-plot = px.line(df, x = 'x', y = ['func1', 'func2', 'func3'])
-
 st.plotly_chart(plot)
