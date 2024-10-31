39fdd9698803f6a8e22e74d1e12ff6c1d150eb78
Wing To <wto@csustan.edu>
2024-10-30 17:53:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,16 @@
-from datetime import datetime, timedelta
-
 import pandas as pd
 import streamlit as st
 from mitosheet.streamlit.v1 import spreadsheet
-from mitosheet.streamlit.v1.spreadsheet import _get_mito_backend
 
-st.set_page_config(layout="wide")
+# Create a dataframe with pandas (you can pass any pandas dataframe)
+dataframe = pd.DataFrame({'A': [1, 2, 3]})
 
-@st.cache_data
-def get_tesla_data():
-    df = pd.read_csv('https://github.com/wtoslac/Streamlit_Tutorial/blob/main/20230101_20241231_Turlock_CA_USA.lev15')
-    df = df.drop(-999)
-    df['AOD_500nm'] = df['AOD_500nm'].astype(float)
-    return df
+# Display the dataframe in a Mito spreadsheet
+final_dfs, code = spreadsheet(dataframe)
 
-tesla_data = get_tesla_data()
+# Display the final dataframes created by editing the Mito component
+# This is a dictionary from dataframe name -> dataframe
+st.write(final_dfs)
 
-new_dfs, code = spreadsheet(tesla_data)
-code = code if code else "# Edit the spreadsheet above to generate code"
+# Display the code that corresponds to the script
 st.code(code)
-
-def clear_mito_backend_cache():
-    _get_mito_backend.clear()
-
-# Function to cache the last execution time - so we can clear periodically
-@st.cache_resource
-def get_cached_time():
-    # Initialize with a dictionary to store the last execution time
-    return {"last_executed_time": None}
-
-def try_clear_cache():
-
-    # How often to clear the cache
-    CLEAR_DELTA = timedelta(hours=12)
-
-    current_time = datetime.now()
-    cached_time = get_cached_time()
-
-    # Check if the current time is different from the cached last execution time
-    if cached_time["last_executed_time"] is None or cached_time["last_executed_time"] + CLEAR_DELTA < current_time:
-        clear_mito_backend_cache()
-        cached_time["last_executed_time"] = current_time
-
-try_clear_cache()
