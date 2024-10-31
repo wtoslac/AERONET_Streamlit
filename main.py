8e51955df8557c2fd98dc6dbb70a2653d1f550c9
Wing To <wto@csustan.edu>
2024-10-30 18:15:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import streamlit as st
 import numpy as np
 import plotly.express as px
 import pandas as pd
- 
-x = np.linspace(0, 5)
- 
-y1 = np.sin(x - 1) + np.cos(x)
-y2 = np.sin(x * x) * np.exp(-0.1 *  x * x)
-y3 = np.cos(x * x) * np.exp(0.1 * x)
- 
-df = pd.DataFrame(dict(x = x, func1 = y1, func2 = y2, func3 = y3))
+from io import StringIO 
+import numpy as np
+import plotly.express as px
+
+file = st.file_uploader("Please choose a file")
+df= pd.read_csv(file)
+plot = px.line(df, x = 'datetime', y = ['AOD_500nm'])
+st.plotly_chart(plot)
  
 plot = px.line(df, x = 'x', y = ['func1', 'func2', 'func3'])
 
