1247a829458895a116c106262fe735871ab4b66b
Wing To <wto@csustan.edu>
2024-10-30 18:01:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,57 @@
-import pandas as pd
+importing required libraries
+
 import streamlit as st
-from mitosheet.streamlit.v1 import spreadsheet
 
-# Create a dataframe with pandas (you can pass any pandas dataframe)
-dataframe = pd.DataFrame({'A': [1, 2, 3]})
+from io import StringIO 
 
-# Display the dataframe in a Mito spreadsheet
-final_dfs, code = spreadsheet(dataframe)
 
-# Display the final dataframes created by editing the Mito component
-# This is a dictionary from dataframe name -> dataframe
-st.write(final_dfs)
 
-# Display the code that corresponds to the script
-st.code(code)
+#adding a file uploader
+
+file = st.file_uploader("Please choose a file")
+
+if file is not None:
+
+    #To read file as bytes:
+
+    bytes_data = file.getvalue()
+
+    st.write(bytes_data)
+
+
+
+    #To convert to a string based IO:
+
+    stringio = StringIO(file.getvalue().decode("utf-8"))
+
+    st.write(stringio)
+
+
+
+    #To read file as string:
+
+    string_data = stringio.read()
+
+    st.write(string_data)
+
+
+
+    #Can be used wherever a "file-like" object is accepted:
+
+    df= pd.read_csv(file)
+
+    st.write(df)
+
+
+
+#adding a file uploader to accept multiple CSV files
+
+uploaded_files = st.file_uploader("Please choose a CSV file", accept_multiple_files=True)
+
+for file in uploaded_files:
+
+    bytes_data = file.read()
+
+    st.write("File uploaded:", file.name)
+
+    st.write(bytes_data)
