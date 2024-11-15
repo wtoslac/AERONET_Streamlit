02d227576e36276fd1b2cdfc2659fd812f1efb55
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-15 13:45:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ for color in colors:
             continue
         if wavelength in wavelengths:
             user_matches[color] = wavelength
-            st.text(f"Great job this is correct! {color} = {correct_matches}")
+            st.text(f"Great job this is correct! {color} = {correct_matches[{color}]}  ")
             break
         elif not error_message_shown:
             st.text(f"Invalid choice for {color}. Please choose one of 380, 500, or 870.")
