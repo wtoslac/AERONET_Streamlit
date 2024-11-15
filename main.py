b0add83c4c0e041f47a9588bc5dbbeaf37f47b96
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-15 12:28:35 -0800

    Update main.py

--- main.py
+++ main.py
@@ -11,16 +11,15 @@ StartDate = st.date_input("StartDate", datetime.date(2024, 10, 1))
 StartDateTime = datetime.datetime.combine(StartDate, datetime.time(0,0))
 EndDate = st.date_input("EndDate", datetime.date(2024, 10, 7))
 EndDateTime = datetime.datetime.combine(EndDate, datetime.time(23,59))
-# Add number input fields for ymin and ymax
-AOD_min = st.number_input("Set minimum AOD value (y-min)", value=0.0, step=0.01)
-AOD_max = st.number_input("Set maximum AOD value (y-max)", value=0.3, step=0.01)
+AOD_min = 0.0
+AOD_max = 0.3
 
-file = st.file_uploader("20230101_20241231_Turlock_CA_USA.zip")
+file = st.file_uploader("Upload the Level 1.5 Data Downloaded from: https://aeronet.gsfc.nasa.gov/cgi-bin/webtool_aod_v3?stage=3&region=United_States_West&state=California&site=Turlock_CA_USA")
 df = pd.read_csv(file,skiprows = 6, parse_dates={'datetime':[0,1]})
 datetime_utc=pd.to_datetime(df["datetime"], format='%d:%m:%Y %H:%M:%S')
 datetime_pac= pd.to_datetime(datetime_utc).dt.tz_localize('UTC').dt.tz_convert('US/Pacific')
 df.set_index(datetime_pac, inplace = True)
-plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'),"AOD_500nm"].resample(SampleRate).mean(),'.k',label="AOD_500nm")
+plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'),"AOD_500nm","AOD_850nm","AOD_480nm"].resample(SampleRate).mean(),'.k',label="AOD_500nm")
 
 plt.gcf().autofmt_xdate()
 plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1, tz='US/Pacific'))
