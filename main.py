d46fa8e62cb70bdb7586b1583143bfffe463da00
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-18 12:12:31 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,93 +1,34 @@
+import streamlit as st
+import datetime
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-import streamlit as st
-import datetime
 
-# Set up Streamlit inputs
-siteName = "Turlock CA USA"
+siteName="Turlock CA USA"
 SampleRate = "1h"
 StartDate = st.date_input("StartDate", datetime.date(2024, 10, 1))
-StartDateTime = datetime.datetime.combine(StartDate, datetime.time(0, 0))
+StartDateTime = datetime.datetime.combine(StartDate, datetime.time(0,0))
 EndDate = st.date_input("EndDate", datetime.date(2024, 10, 7))
-EndDateTime = datetime.datetime.combine(EndDate, datetime.time(23, 59))
+EndDateTime = datetime.datetime.combine(EndDate, datetime.time(23,59))
 AOD_min = 0.0
-AOD_max = 0.4
+AOD_max = 0.3
 
-# Upload AERONET and Wind data files
-aeronet_file = st.file_uploader("Upload AERONET Level 1.5 Data")
-wind_file = st.file_uploader("Upload Wind Data")
+file = st.file_uploader("Upload the Level 1.5 Data Downloaded from: https://aeronet.gsfc.nasa.gov/cgi-bin/webtool_aod_v3?stage=3&region=United_States_West&state=California&site=Turlock_CA_USA")
+df = pd.read_csv(file,skiprows = 6, parse_dates={'datetime':[0,1]})
+datetime_utc=pd.to_datetime(df["datetime"], format='%d:%m:%Y %H:%M:%S')
+datetime_pac= pd.to_datetime(datetime_utc).dt.tz_localize('UTC').dt.tz_convert('US/Pacific')
+df.set_index(datetime_pac, inplace = True)
+plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'),"AOD_380nm"].resample(SampleRate).mean(),'.b',label="AOD_380nm")
+plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'),"AOD_500nm"].resample(SampleRate).mean(),'.g',label="AOD_500nm")
+plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'),"AOD_870nm"].resample(SampleRate).mean(),'.r',label="AOD_870nm")
 
-if aeronet_file and wind_file:
-    # Process AERONET data
-    df = pd.read_csv(aeronet_file, skiprows=6, parse_dates={'datetime': [0, 1]})
-    datetime_utc = pd.to_datetime(df["datetime"], format='%d:%m:%Y %H:%M:%S')
-    datetime_pac = datetime_utc.dt.tz_localize('UTC').dt.tz_convert('US/Pacific')
-    df.set_index(datetime_pac, inplace=True)
 
-    # Process Wind Data
-    wind_df = pd.read_csv(wind_file, parse_dates={'datetime': [1]}, low_memory=False)
-    wind_datetime_utc = pd.to_datetime(wind_df["datetime"], format='%d-%m-%Y %H:%M:%S')
-    wind_datetime_pac = wind_datetime_utc.dt.tz_localize('UTC').dt.tz_convert('US/Pacific')
-    wind_df.set_index(wind_datetime_pac, inplace=True)
-    wind_df = wind_df.loc[StartDateTime:EndDateTime]
-
-    # Extract wind speed and direction
-    wind_speed = wind_df['WND'].str.split(',', expand=True)[3].astype(float) / 10.0
-    wind_direction = wind_df['WND'].str.split(',', expand=True)[0].astype(float)
-
-    # Convert wind data to Cartesian coordinates
-    wind_x = wind_speed * np.sin(np.radians(wind_direction))
-    wind_y = wind_speed * np.cos(np.radians(wind_direction))
-
-    # Create the plot
-    fig, ax = plt.subplots(figsize=(12, 6))
-
-    # Plot AOD data
-    ax.plot(
-        df.loc[StartDateTime:EndDateTime, "AOD_500nm"].resample(SampleRate).mean(),
-        label="AOD_500nm",
-        marker="o",
-        linestyle="-",
-        color="black",
-    )
-    ax.set_ylabel("AOD (500 nm)")
-    ax.set_ylim(AOD_min, AOD_max)
-
-    # Add temperature data
-    temp_ax = ax.twinx()
-    temp_data = wind_df['TMP'].str.split(',', expand=True)[0].astype(float) / 10.0
-    temp_ax.plot(
-        temp_data.resample(SampleRate).mean(),
-        label="Temperature",
-        linestyle="--",
-        color="red",
-    )
-    temp_ax.set_ylabel("Temperature (°C)", color="red")
-    temp_ax.tick_params(axis='y', labelcolor="red")
-
-    # Add wind quiver
-    quiver_ax = ax.twinx()
-    quiver_ax.spines["right"].set_position(("axes", 1.1))  # Offset for better visualization
-    quiver_ax.quiver(
-        wind_x.index, np.zeros_like(wind_x),
-        wind_x.resample(SampleRate).mean(), wind_y.resample(SampleRate).mean(),
-        angles="xy", scale_units="xy", scale=1, color="blue", label="Wind Vector"
-    )
-    quiver_ax.set_ylabel("Wind (m/s)", color="blue")
-    quiver_ax.tick_params(axis='y', labelcolor="blue")
-
-    # Formatting and legend
-    ax.xaxis.set_major_locator(mdates.DayLocator(interval=1, tz='US/Pacific'))
-    ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
-    ax.grid(True, which="both", linestyle="--", linewidth=0.5)
-    fig.autofmt_xdate()
-
-    # Legend for all data
-    lines, labels = ax.get_legend_handles_labels()
-    lines2, labels2 = temp_ax.get_legend_handles_labels()
-    lines3, labels3 = quiver_ax.get_legend_handles_labels()
-    ax.legend(lines + lines2 + lines3, labels + labels2 + labels3, loc="best")
-
-    st.pyplot(fig)
+plt.gcf().autofmt_xdate()
+plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1, tz='US/Pacific'))
+plt.gca().xaxis.set_minor_locator(mdates.HourLocator(interval=12, tz='US/Pacific'))
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+#Change the range on Y here if needed
+plt.ylim(AOD_min,AOD_max)
+plt.legend()
+st.pyplot(plt.gcf())
