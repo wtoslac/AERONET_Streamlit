e3cc10ecd0428dc8fd5c32f1b9bea3d40b93f54d
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-18 10:46:20 -0800

    Update main.py

--- main.py
+++ main.py
@@ -26,15 +26,15 @@ wind_file = st.file_uploader("Upload Wind Data (CSV)")
 
 if aod_file is not None and wind_file is not None:
     # Read AOD data
-    df_aod = pd.read_csv(aod_file, skiprows=6, parse_dates={'Date': [0, 1]})
-    datetime_utc = pd.to_datetime(df_aod["Date"], format='%d:%m:%Y %H:%M:%S')
+    df_aod = pd.read_csv(aod_file, skiprows=6, parse_dates={'datetime': [0, 1]})
+    datetime_utc = pd.to_datetime(df_aod["datetime"], format='%d:%m:%Y %H:%M:%S')
     datetime_pac = pd.to_datetime(datetime_utc).dt.tz_localize('UTC').dt.tz_convert('US/Pacific')
     df_aod.set_index(datetime_pac, inplace=True)
 
     # Read Wind data
-    df_wind = pd.read_csv(wind_file, parse_dates=['Datetime'])
-    df_wind['datetime'] = pd.to_datetime(df_wind['Datetime']).dt.tz_localize('UTC').dt.tz_convert('US/Pacific')
-    df_wind.set_index('datetime', inplace=True)
+    df_wind = pd.read_csv(wind_file, parse_dates=['date'])
+    df_wind['datetime'] = pd.to_datetime(df_wind['date']).dt.tz_localize('UTC').dt.tz_convert('US/Pacific')
+    df_wind.set_index('date', inplace=True)
 
     # Plot AOD and wind data
     fig, ax1 = plt.subplots(figsize=(10, 6))
