987515e45226d3cf039d1ae7f3f44b767ccedee3
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-19 13:19:04 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,65 +5,30 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
-# Set up basic information
-siteName = "Turlock CA USA"
+siteName="Turlock CA USA"
 SampleRate = "1h"
 StartDate = st.date_input("StartDate", datetime.date(2024, 10, 1))
-StartDateTime = datetime.datetime.combine(StartDate, datetime.time(0, 0))
+StartDateTime = datetime.datetime.combine(StartDate, datetime.time(0,0))
 EndDate = st.date_input("EndDate", datetime.date(2024, 10, 7))
-EndDateTime = datetime.datetime.combine(EndDate, datetime.time(23, 59))
+EndDateTime = datetime.datetime.combine(EndDate, datetime.time(23,59))
 AOD_min = 0.0
-AOD_max = 0.4
+AOD_max = 0.3
 
-# Upload file
-file = st.file_uploader("Upload the Level 1.5 Data from AERONET")
-if file is not None:
-    df = pd.read_csv(file, skiprows=6, parse_dates={'datetime': [0, 1]})
-    datetime_utc = pd.to_datetime(df["datetime"], format='%d:%m:%Y %H:%M:%S')
-    datetime_pac = pd.to_datetime(datetime_utc).dt.tz_localize('UTC').dt.tz_convert('US/Pacific')
-    df.set_index(datetime_pac, inplace=True)
+file = st.file_uploader("Upload the Level 1.5 Data Downloaded from: https://aeronet.gsfc.nasa.gov/cgi-bin/webtool_aod_v3?stage=3&region=United_States_West&state=California&site=Turlock_CA_USA")
+df = pd.read_csv(file,skiprows = 6, parse_dates={'datetime':[0,1]})
+datetime_utc=pd.to_datetime(df["datetime"], format='%d:%m:%Y %H:%M:%S')
+datetime_pac= pd.to_datetime(datetime_utc).dt.tz_localize('UTC').dt.tz_convert('US/Pacific')
+df.set_index(datetime_pac, inplace = True)
+plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'),"AOD_380nm"].resample(SampleRate).mean(),'.b',label="AOD_380nm")
+plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'),"AOD_500nm"].resample(SampleRate).mean(),'.g',label="AOD_500nm")
+plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'),"AOD_870nm"].resample(SampleRate).mean(),'.r',label="AOD_870nm")
 
-    # Matching wavelengths to positions
-    st.text("\nNow set start date to 2024/10/01 you can see three different data clusters for 10/01. Now Match the wavelength to its position:")
-    positions = ["Top", "Middle", "Bottom"]
 
-    # Dropdown menus for user input with no default selection
-    user_matches = {}
-    for pos in positions:
-        user_matches[pos] = st.selectbox(f"Wavelength for {pos} position:", 
-                                         options=["Select an option", "450 nm", "500 nm", "870 nm"], 
-                                         key=pos)
-
-    # Define color mapping
-    color_mapping = {
-        "450 nm": "purple",
-        "500 nm": "green",
-        "870 nm": "red"
-    }
-
-    # Allow user to proceed without showing correctness
-    if st.button("Submit"):
-        st.text("Your selections have been recorded. Take a screenshot and submit your answer! You can proceed to the next step.")
-
-        # Plot data with the selected colors
-        plt.figure(figsize=(10, 6))
-
-        # Loop over the user selections and plot corresponding wavelengths
-        for pos, wavelength in user_matches.items():
-            if wavelength != "Select an option":  # Only plot if the user has selected a valid option
-                color = color_mapping.get(wavelength, 'black')  # Default to black if no color is found
-                column_name = f"AOD_{wavelength.replace(' ', '').replace('nm', '')}"  # Create column name based on the selected wavelength
-                if column_name in df.columns:  # Check if the column exists
-                    plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), column_name].resample(SampleRate).mean(),
-                             color=color, label=f"AOD {wavelength}")
-                else:
-                    st.warning(f"Column {column_name} not found in the dataset.")
-
-        # Set plot aesthetics
-        plt.gcf().autofmt_xdate()
-        plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1, tz='US/Pacific'))
-        plt.gca().xaxis.set_minor_locator(mdates.HourLocator(interval=12, tz='US/Pacific'))
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
-        plt.ylim(AOD_min, AOD_max)
-        plt.legend()
-        st.pyplot(plt.gcf())
+plt.gcf().autofmt_xdate()
+plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1, tz='US/Pacific'))
+plt.gca().xaxis.set_minor_locator(mdates.HourLocator(interval=12, tz='US/Pacific'))
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+#Change the range on Y here if needed
+plt.ylim(AOD_min,AOD_max)
+plt.legend()
+st.pyplot(plt.gcf())
