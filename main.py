4eaf9ffc258bcdf7215588d9206c01f974eed793
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-19 13:14:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -37,49 +37,25 @@ if file is not None:
     st.pyplot(plt.gcf())
 
 # Matching wavelengths to positions
-st.text("\nNow set start date to 2024/10/01. You can see three different data clusters for 10/01. Now match the wavelength to its position:")
+st.text("\n Now set start date to 2024/10/01 you can see three diffrent data clusters for 10/01.Now Match the wavelength to its position:")
 positions = ["Top", "Middle", "Bottom"]
 
 # Dropdown menus for user input with no default selection
 user_matches = {}
 for pos in positions:
-    user_matches[pos] = st.selectbox(f"Wavelength for {pos} position:", 
+    user_matches[pos] = st.selectbox(f"wavelength for {pos} position:", 
                                      options=["Select an option", "450 nm", "500 nm", "870 nm"], 
                                      key=pos)
 
 # Allow user to proceed without showing correctness
 if st.button("Submit"):
-    st.text("Your selections have been recorded. Take a screenshot and submit your answer! You can proceed to the next step.")
+    st.text("Your selections have been recorded.Take screenshot and submit answer!.You can proceed to the next step.")
 
-    # Create the plot with user color preferences based on wavelength
-    colors = {
-        "450 nm": "purple",
-        "500 nm": "green",
-        "870 nm": "red"
-    }
-    
-    # Define wavelength data mapping based on user selections
-    wavelength_map = {
-        "Top": user_matches["Top"],
-        "Middle": user_matches["Middle"],
-        "Bottom": user_matches["Bottom"]
-    }
-    
-    # Plot data with appropriate colors for each wavelength
-    plt.figure(figsize=(10, 6))
-    for position, wavelength in wavelength_map.items():
-        if wavelength != "Select an option":
-            if wavelength == "450 nm":
-                plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_450nm"].resample(SampleRate).mean(), color=colors[wavelength], label=f"AOD {wavelength}")
-            elif wavelength == "500 nm":
-                plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_500nm"].resample(SampleRate).mean(), color=colors[wavelength], label=f"AOD {wavelength}")
-            elif wavelength == "870 nm":
-                plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_870nm"].resample(SampleRate).mean(), color=colors[wavelength], label=f"AOD {wavelength}")
-    
-    plt.gcf().autofmt_xdate()
-    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1, tz='US/Pacific'))
-    plt.gca().xaxis.set_minor_locator(mdates.HourLocator(interval=12, tz='US/Pacific'))
-    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
-    plt.ylim(AOD_min, AOD_max)
-    plt.legend()
-    st.pyplot(plt.gcf())
+file = st.file_uploader("Upload the Level 1.5 Data Downloaded from: https://aeronet.gsfc.nasa.gov/cgi-bin/webtool_aod_v3?stage=3&region=United_States_West&state=California&site=Turlock_CA_USA")
+df = pd.read_csv(file,skiprows = 6, parse_dates={'datetime':[0,1]})
+datetime_utc=pd.to_datetime(df["datetime"], format='%d:%m:%Y %H:%M:%S')
+datetime_pac= pd.to_datetime(datetime_utc).dt.tz_localize('UTC').dt.tz_convert('US/Pacific')
+df.set_index(datetime_pac, inplace = True)
+plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'),"AOD_380nm"].resample(SampleRate).mean(),'.b',label="AOD_380nm")
+plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'),"AOD_500nm"].resample(SampleRate).mean(),'.g',label="AOD_500nm")
+plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'),"AOD_870nm"].resample(SampleRate).mean(),'.r',label="AOD_870nm")
