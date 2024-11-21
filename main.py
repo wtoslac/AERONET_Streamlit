2eaaba682c9fb82cb0099243337fc2b3a513556b
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-20 18:35:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -17,45 +17,63 @@ st.sidebar.header("Adjust Y-axis Limits")
 AOD_min = st.sidebar.slider("Y-Axis Min", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
 AOD_max = st.sidebar.slider("Y-Axis Max", min_value=0.0, max_value=1.0, value=0.3, step=0.01)
 
-# File uploads
-file1 = st.file_uploader("Upload the first CSV file (first_half.csv)")
-file2 = st.file_uploader("Upload the second CSV file (second_half.csv)")
+# File upload
+file = st.file_uploader("Upload the Level 1.5 Data from AERONET")
+if file is not None:
+    # Read the data
+    df = pd.read_csv(file, skiprows=6, parse_dates={'datetime': [0, 1]})
+    datetime_utc = pd.to_datetime(df["datetime"], format='%d:%m:%Y %H:%M:%S')
+    datetime_pac = pd.to_datetime(datetime_utc).dt.tz_localize('UTC').dt.tz_convert('US/Pacific')
+    df.set_index(datetime_pac, inplace=True)
 
-if file1 and file2:
-    try:
-        # Load both files
-        df1 = pd.read_csv(file1, skiprows=6, parse_dates={'datetime': [0, 1]})
-        df2 = pd.read_csv(file2, skiprows=6, parse_dates={'datetime': [0, 1]})
+    # Plot initial graph in black and white (no color coding)
+    plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_380nm"].resample(SampleRate).mean(), '.k')
+    plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_500nm"].resample(SampleRate).mean(), '.k')
+    plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_870nm"].resample(SampleRate).mean(), '.k')
 
-        # Debugging: Display sample data
-        st.write("First file sample:")
-        st.write(df1.head())
-        st.write("Second file sample:")
-        st.write(df2.head())
+    # Format the initial plot
+    plt.gcf().autofmt_xdate()
+    plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1, tz='US/Pacific'))
+    plt.gca().xaxis.set_minor_locator(mdates.HourLocator(interval=12, tz='US/Pacific'))
+    plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+    plt.ylim(AOD_min, AOD_max)
+    plt.legend()
+    st.pyplot(plt.gcf())
 
-        # Concatenate the data
-        df = pd.concat([df1, df2], ignore_index=True)
+    # Ask user to match wavelengths to positions
+    st.text("\nMatch the wavelengths to the positions on the graph:")
 
-        # Parse and set datetime as index
-        df["datetime"] = pd.to_datetime(df["datetime"], errors='coerce')
-        df.set_index("datetime", inplace=True)
+    # Dropdown menus for user input with no default selection
+    positions = ["Top", "Middle", "Bottom"]
 
-        # Plot data
-        plt.figure(figsize=(10, 6))
-        plt.plot(df.loc[StartDateTime:EndDateTime, "AOD_380nm"].resample(SampleRate).mean(), '.k', label="380 nm")
-        plt.plot(df.loc[StartDateTime:EndDateTime, "AOD_500nm"].resample(SampleRate).mean(), '.k', label="500 nm")
-        plt.plot(df.loc[StartDateTime:EndDateTime, "AOD_870nm"].resample(SampleRate).mean(), '.k', label="870 nm")
+    # Create user input dropdowns
+    user_matches = {}
+    for pos in positions:
+        user_matches[pos] = st.selectbox(f"What Wavelength will be located on the {pos} position on the graph?", 
+                                         options=["Select an option", "400 nm", "500 nm", "779 nm"], 
+                                         key=pos)
 
-        # Format the plot
+    # Allow user to submit and display feedback
+    if st.button("Submit"):
+        st.text("Your selections have been recorded. Take a screenshot and submit your answer!")
+
+        # Create a second graph with predefined colors for the wavelengths (independent of user's input)
+        wavelength_colors = {
+            "380 nm": "b",  # Red
+            "500 nm": "g",  # Green
+            "870 nm": "r"   # Blue
+        }
+
+        # Create the second graph independently of user input
+        plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_380nm"].resample(SampleRate).mean(), '.b', color=wavelength_colors["380 nm"], label="400 nm")
+        plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_500nm"].resample(SampleRate).mean(), '.g', color=wavelength_colors["500 nm"], label="500 nm")
+        plt.plot(df.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_870nm"].resample(SampleRate).mean(), '.r', color=wavelength_colors["870 nm"], label="779 nm")
+
+        # Format the second plot
         plt.gcf().autofmt_xdate()
-        plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1))
-        plt.gca().xaxis.set_minor_locator(mdates.HourLocator(interval=12))
+        plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1, tz='US/Pacific'))
+        plt.gca().xaxis.set_minor_locator(mdates.HourLocator(interval=12, tz='US/Pacific'))
         plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
         plt.ylim(AOD_min, AOD_max)
         plt.legend()
-        plt.title(f"AOD Measurements for {siteName}")
-        plt.xlabel("Date")
-        plt.ylabel("AOD")
         st.pyplot(plt.gcf())
-
-    
