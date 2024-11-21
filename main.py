9dd671d0a1964d2e459cdc71b76c1cafdb8714fe
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-20 21:27:53 -0800

    Update main.py

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ if df_1 is not None:
         st.error(f"Missing expected columns in the dataset. Available columns: {df_1.columns}")
     
     # Plot data from the first repository if columns are correct
-    if 'AOD_440nm' in df_1.columns and 'AOD_500nm' in df_1.columns and 'AOD_870nm' in df_1.columns:
+    if 'AOD_440nm' in df_1.columns and 'AOD_500nm' in df_1.columns and 'AOD_681nm' in df_1.columns:
         
         # Plot AOD_380nm, AOD_500nm, and AOD_870nm
         plt.plot(df_1.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_440nm"].resample(SampleRate).mean(), '.b', label="440 nm")
