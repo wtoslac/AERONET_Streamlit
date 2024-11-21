dc472ead9ab4771ff6698da39c8ff9b360c5c4d1
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-21 12:20:07 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import numpy as np
 # Set up basic information
 siteName = "Turlock CA USA"
 SampleRate = "1h"
-st.tittle = ("AOD Measurments for Turlcok CA")
+st.title = ("AOD Measurments for Turlcok CA")
 StartDate = st.date_input("StartDate", datetime.date(2023, 7, 1))
 StartDateTime = datetime.datetime.combine(StartDate, datetime.time(0, 0))
 EndDate = st.date_input("EndDate", datetime.date(2023, 7,7))
