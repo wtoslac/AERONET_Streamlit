171a69a7c8eadee8757d7e2d8d99bfa35b57a992
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-20 21:29:25 -0800

    Update main.py

--- main.py
+++ main.py
@@ -53,12 +53,12 @@ if df_1 is not None:
         st.error(f"Missing expected columns in the dataset. Available columns: {df_1.columns}")
     
     # Plot data from the first repository if columns are correct
-    if 'AOD_440nm' in df_1.columns and 'AOD_500nm' in df_1.columns and 'AOD_681nm' in df_1.columns:
+    if 'AOD_440nm' in df_1.columns and 'AOD_500nm' in df_1.columns and 'AOD_779nm' in df_1.columns:
         
         # Plot AOD_380nm, AOD_500nm, and AOD_870nm
         plt.plot(df_1.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_440nm"].resample(SampleRate).mean(), '.b', label="440 nm")
         plt.plot(df_1.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_500nm"].resample(SampleRate).mean(), '.g', label="500 nm")
-        plt.plot(df_1.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_681nm"].resample(SampleRate).mean(), '.r', label="681 nm")
+        plt.plot(df_1.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_779nm"].resample(SampleRate).mean(), '.r', label="779 nm")
 
         # Format the plot
         plt.gcf().autofmt_xdate()
