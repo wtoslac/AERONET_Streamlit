ae5e4342d793e5694facd721c2b811d609998863
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-22 08:37:01 -0800

    Update main.py

--- main.py
+++ main.py
@@ -176,8 +176,6 @@ st.pyplot(fig)
 # Convert StartDate and EndDate to datetime objects
 #StartDate = pd.to_datetime('2023-07-01')
 #EndDate = pd.to_datetime('2023-07-07')
-StartDate='2023-10-07 00:00:00'
-EndDate='2023-10-15 23:59:59'
 windfile = 'https://raw.githubusercontent.com/Rsaltos7/AERONET_Streamlit/refs/heads/main/Modesto_Wind_2023%20(2).csv'
 windSampleRate = '3h'
 StartDate='2023-10-07 00:00:00'
