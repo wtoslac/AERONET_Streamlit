810521632f1ae24fbdd91b1af32684dd49eee73d
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-22 08:13:06 -0800

    Update main.py

--- main.py
+++ main.py
@@ -172,8 +172,8 @@ plt.tight_layout()
 # Display the plot in Streamlit
 st.pyplot(fig)
 
-StartDate='2023-07-01'
-EndDate='2023-07-07'
+StartDate = pd.to_datetime('2023-07-01')
+EndDate = pd.to_datetime('2023-07-07')
 # Assuming Wdf, StartDate, EndDate, siteName, filename, SampleRate are defined earlier
 Tdf = Wdf.loc[StartDate:EndDate, 'TMP'].str.split(pat=',', expand=True)
 Tdf.replace('+9999', np.nan, inplace=True)
