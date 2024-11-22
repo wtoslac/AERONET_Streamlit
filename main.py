175265dc409e2dc2baad2c0b9beded2a56e7d75b
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-22 08:44:11 -0800

    Update main.py

--- main.py
+++ main.py
@@ -196,7 +196,10 @@ temp_data = Tdf.loc[StartDate:EndDate].astype(float).resample(SampleRate).mean()
 #y_min =17 #temp_data.min() - 1  # Add a small buffer below minimum
 #y_max =24 #temp_data.max() + 1  # Add a small buffer above maximum
 ax.set_ylabel('Temperature (°C)')
-ax.set_ylim(14,50)
+ax.set_ylim(Tdf[0].loc[StartDate:EndDate].astype(float).resample(SampleRate).mean().div(10).min()//1,
+            Tdf[0].loc[StartDate:EndDate].astype(float).resample(SampleRate).mean().div(10).max()//1) # Auto Calculating
+temperatureHandle, = ax.plot(Tdf[0].loc[StartDate:EndDate].astype(float).resample(SampleRate).mean().div(10), '.r-',label='Temperature',figure=fig) # handle, label = ax2.plot()
+
 
 # Plot the data
 ax.plot(temp_data, '.r-', label='Temperature')
