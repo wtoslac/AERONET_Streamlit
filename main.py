b90f3bab050fe438e2cbb05e8f317709d4039ceb
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-22 10:11:03 -0800

    Update main.py

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ Tdf = Wdf.loc[StartDate:EndDate, 'TMP'].str.split(pat=',', expand=True)
 Tdf.replace('+9999', np.nan, inplace=True)
 
 # Create subplots (2x2 layout, adjust as needed)
-fig, axes = plt.subplots(1, 1, figsize=(10, 6))
+fig, axes = plt.subplots(1, 1, figsize=(13, 8))
 #ax = axes[0, 0]  # Use the first subplot for this specific plot
 
 # Format the figure and axis
