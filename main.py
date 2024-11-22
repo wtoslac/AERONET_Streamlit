1d8af77056d45bfdacc8c3309158d37360afb50c
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-22 14:22:57 -0800

    Update main.py

--- main.py
+++ main.py
@@ -186,7 +186,7 @@ except:
         ax = axes # If axes is just a single axis then we'll use it directly.
 # Initializing main Axis and plot
 fig.autofmt_xdate() ## Note: With multiple plots, this removes the x-axis identifiers for plots not in the bottom row
-ax.set_title(siteName + ' ' + filename[0:4] + ' Temperature')
+ax.set_title('Modesto Temperature')
 ax.grid(which='both',axis='both')
 ax.xaxis.set_major_locator(mdates.DayLocator(interval=1, tz='US/Pacific'))
 ax.xaxis.set_minor_locator(mdates.HourLocator(interval=3, tz='US/Pacific'))
