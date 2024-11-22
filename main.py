865486bf77bc5499e895732f99d01fbcf6e356f6
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-22 14:35:13 -0800

    Update main.py

--- main.py
+++ main.py
@@ -145,10 +145,10 @@ except:
 # Initializing main Axis and plot
 fig.autofmt_xdate() ## Note: With multiple plots, this removes the x-axis identifiers for plots not in the bottom row
 ax.set_title('Modesto Temperature')
-ax.grid(which='both',axis='both')
-ax.xaxis.set_major_locator(mdates.DayLocator(interval=1, tz='US/Pacific'))
-ax.xaxis.set_minor_locator(mdates.HourLocator(interval=3, tz='US/Pacific'))
-ax.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+ax3.grid(which='both',axis='both')
+ax3.xaxis.set_major_locator(mdates.DayLocator(interval=1, tz='US/Pacific'))
+ax3.xaxis.set_minor_locator(mdates.HourLocator(interval=3, tz='US/Pacific'))
+ax3.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
 
 # Drawing the Temperature Data onto the graph.
 ax.set_ylabel('Temperature °C')
@@ -211,35 +211,35 @@ plt.tight_layout()
 st.pyplot(fig)
 
 #Temp
-Tdf = Wdf.loc[StartDate:EndDate,'TMP'].str.split(pat=',', expand = True)
+#Tdf = Wdf.loc[StartDate:EndDate,'TMP'].str.split(pat=',', expand = True)
 ## Replacing +9999 values with nan, +9999 indicates "missing data"
-Tdf.replace('+9999', np.nan, inplace = True)
-fig, axes = plt.subplots(1,1, figsize=(16,9)) # plt.subplots(nrows, ncolumns, *args) # axs will be either an individual plot or an array of axes
-try:
-    ax = axes[0,0] # If axes is a 2D array of axes, then we'll use the first axis for this drawing.
-except:
-    try:
-        ax = axes[0] # If axes is a 1D array of axes, then we'll use the first axis for this drawing.
-    except:
-        ax = axes # If axes is just a single axis then we'll use it directly.
+#Tdf.replace('+9999', np.nan, inplace = True)
+#fig, axes = plt.subplots(1,1, figsize=(16,9)) # plt.subplots(nrows, ncolumns, *args) # axs will be either an individual plot or an array of axes
+#try:
+#    ax = axes[0,0] # If axes is a 2D array of axes, then we'll use the first axis for this drawing.
+#except:
+#    try:
+#        ax = axes[0] # If axes is a 1D array of axes, then we'll use the first axis for this drawing.
+#    except:
+      #  ax = axes # If axes is just a single axis then we'll use it directly.
 # Initializing main Axis and plot
-fig.autofmt_xdate() ## Note: With multiple plots, this removes the x-axis identifiers for plots not in the bottom row
-ax3.set_title('Modesto Temperature')
-ax3.grid(which='both',axis='both')
-ax3.xaxis.set_major_locator(mdates.DayLocator(interval=1, tz='US/Pacific'))
-ax3.xaxis.set_minor_locator(mdates.HourLocator(interval=3, tz='US/Pacific'))
-ax3.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
+#fig.autofmt_xdate() ## Note: With multiple plots, this removes the x-axis identifiers for plots not in the bottom row
+#ax3.set_title('Modesto Temperature')
+#ax3.grid(which='both',axis='both')
+#ax3.xaxis.set_major_locator(mdates.DayLocator(interval=1, tz='US/Pacific'))
+#ax3.xaxis.set_minor_locator(mdates.HourLocator(interval=3, tz='US/Pacific'))
+#ax3.xaxis.set_major_formatter(mdates.DateFormatter('%m-%d'))
 
 # Drawing the Temperature Data onto the graph.
-ax.set_ylabel('Temperature °C')
-ax3.set_ylim(Tdf[0].loc[StartDate:EndDate].astype(float).resample(SampleRate).mean().div(10).min()//1,
-            Tdf[0].loc[StartDate:EndDate].astype(float).resample(SampleRate).mean().div(10).max()//1) # Auto Calculating
-temperatureHandle, = ax.plot(Tdf[0].loc[StartDate:EndDate].astype(float).resample(SampleRate).mean().div(10), '.r-',label='Temperature',figure=fig) # handle, label = ax2.plot()
+#ax.set_ylabel('Temperature °C')
+#ax3.set_ylim(Tdf[0].loc[StartDate:EndDate].astype(float).resample(SampleRate).mean().div(10).min()//1,
+          #  Tdf[0].loc[StartDate:EndDate].astype(float).resample(SampleRate).mean().div(10).max()//1) # Auto Calculating
+#temperatureHandle, = ax.plot(Tdf[0].loc[StartDate:EndDate].astype(float).resample(SampleRate).mean().div(10), '.r-',label='Temperature',figure=fig) # handle, label = ax2.plot()
 
 # Displaying the legend and Reorganizing everything to fit nicely
 ## Note: temperatureHandle is the handle for the data plot we created.
-ax3.plt.legend(handles = [temperatureHandle], loc = 'best')
-ax3.plt.tight_layout() # Adjusts the boundaries of the figures to ensure everything fits nicely. Can define pads as we we see fit.
+#ax3.plt.legend(handles = [temperatureHandle], loc = 'best')
+#ax3.plt.tight_layout() # Adjusts the boundaries of the figures to ensure everything fits nicely. Can define pads as we we see fit.
 
 # Display the plot in Streamlit
 #st.pyplot(fig)
