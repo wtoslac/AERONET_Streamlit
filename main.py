d6b5ed2ff2cdbd421132d5bc56720d6b4229e50d
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-21 16:48:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -153,6 +153,7 @@ ax.quiver(
 plt.plot(df_1.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_440nm"].resample(SampleRate).mean(), '.b',label="440 nm")
 plt.plot(df_1.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_500nm"].resample(SampleRate).mean(), '.g',label="500 nm")
 plt.plot(df_1.loc[StartDateTime.strftime('%Y-%m-%d %H:%M:%S'):EndDateTime.strftime('%Y-%m-%d %H:%M:%S'), "AOD_675nm"].resample(SampleRate).mean(), '.r',label="675 nm")
+ plt.legend()
 ax.get_yaxis().set_visible(True)
 
 # Display the legend and adjust layout
