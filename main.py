c67549a7d96a910bdf01a4a92b5f830db70c877b
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-22 08:20:08 -0800

    Update main.py

--- main.py
+++ main.py
@@ -179,7 +179,7 @@ EndDate = pd.to_datetime('2023-07-07')
 
 # Assuming Wdf is already loaded as a DataFrame with 'TMP' column
 #Tdf = Wdf.loc[StartDate:EndDate, 'TMP'].str.split(pat=',', expand=True)
-Tdf = Wdf_filtered['WND'].str.split(pat=',', expand=True)
+Tdf = Wdf_filtered
 # Replace '+9999' with NaN and convert to numeric
 Tdf.replace('+9999', np.nan, inplace=True)
 Tdf = Tdf.apply(pd.to_numeric, errors='coerce')  # Convert to numeric, errors to NaN
