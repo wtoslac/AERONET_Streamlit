da6edf70e16d580eeb12895235322572fd9739c2
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-21 23:28:10 -0800

    Update main.py

--- main.py
+++ main.py
@@ -139,10 +139,10 @@ ax.set_ylim(AOD_min,AOD_max)
 ax.set_ylabel("Magnitude m/s")
 ax2 = ax.twinx()
 ax2.set_ylabel("AOD Total")
-ax.yaxis.set_label_position('right')  # Move label to the right
-ax.yaxis.set_ticks_position('right')  # Move ticks to the right
-ax2.yaxis.set_label_position('left')  # Move label to the left
-ax2.yaxis.set_ticks_position('left')  # Move ticks to the left
+#ax.yaxis.set_label_position('right')  # Move label to the right
+#ax.yaxis.set_ticks_position('right')  # Move ticks to the right
+#ax2.yaxis.set_label_position('left')  # Move label to the left
+#ax2.yaxis.set_ticks_position('left')  # Move ticks to the left
 
 
 maxWind = np.sqrt((WNDdf[6].loc[StartDate:EndDate].astype(float).max()/10)**2+
