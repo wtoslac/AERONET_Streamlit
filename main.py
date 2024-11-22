4b5b0bab84383e725286aeb6f5b9a4a62985a97a
Rsaltos7 <Rsaltos@csustan.edu>
2024-11-21 22:24:14 -0800

    Update main.py

--- main.py
+++ main.py
@@ -130,9 +130,9 @@ for _, row in WNDdf.iterrows():
 # Add Cartesian components to the DataFrame
 WNDdf[5], WNDdf[6] = Xdata, Ydata
 
-
+graphScale = 1
 # Create a plot
-fig, ax = plt.subplots(figsize=(10, 6))
+fig, ax = plt.subplots(1,1,figsize=(14*graphScale, 7*graphScale))
 ax.set_title("Wind Vector")  # Added title for Wind Vector graph
 ax.set_xlabel("Time")
 ax.set_ylim(AOD_min,AOD_max)
@@ -160,8 +160,7 @@ ax.quiver(
 Tdf = Wdf.loc[StartDate:EndDate,'TMP'].str.split(pat=',', expand = True)
 # Replacing +9999 values with nan, +9999 indicates "missing data"
 Tdf.replace('+9999', np.nan, inplace = True)
-graphScale = 1
-fig, axes = plt.subplots(1,1, figsize=(14*graphScale,7*graphScale)) # plt.subplots(nrows, ncolumns, *args) # axs will be either an individual plot or an array of axes
+fig, axes = plt.subplots(2,2, figsize=(14*graphScale,7*graphScale)) # plt.subplots(nrows, ncolumns, *args) # axs will be either an individual plot or an array of axes
 try:
     ax = axes[0,0] # If axes is a 2D array of axes, then we'll use the first axis for this drawing.
 except:
